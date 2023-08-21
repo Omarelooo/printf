3180b61c795bd3e5862a2aaaf65de64218ccc747
Omarelooo <omark3012@gmail.com>
2023-08-21 06:22:38 -0700

    specifier.c

---
+++ specifier.c
@@ -0,0 +1,116 @@
+#include "main.h"
+
+/**
+ * get_specifier - finds the format func
+ * @s: the format string
+ *
+ * Return: the number of bytes printed
+ */
+
+int (*get_specifier(char *s))(va_list ap, params *params)
+{
+	specifier_t specifier[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifier[i].specifier)
+	{
+		if (*s == specifier[i].specifier[0])
+		{
+			return (specifier[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * get_print_func - finds the format func
+ * @s: the format string
+ * @ap: argument pointer
+ * @params: parameters struct
+ *
+ * Return: the number of bytes printed
+ */
+
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return(f(ap, params));
+}
+
+/**
+ * get_flag - finds the flag func
+ * @s: the format string
+ * @params: paramaters struct
+ *
+ * Return: if flag was valid
+ */
+
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch(*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (1);
+}
+
+/**
+ * get_width -gets the width from the format string
+ * @s: the format string
+ * @params: parameters pointer
+ * @ap: argument pointer
+ *
+ * Return: new pointer
+ */
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_list(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
